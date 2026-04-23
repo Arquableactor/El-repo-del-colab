f9023ef5f7ecdb07edcdc130571ac82fbd1d453f
Arquableactor <Galvajoshua1@gmail.com>
2026-04-23 18:42:31 -0400

    Y como dijo mi ex, hasta aqui llegamos

--- Flowcash-frontend/vite.config.js
+++ Flowcash-frontend/vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-base: "/El-repo-del-colab/flowcash-frontend/"}
-)
+  base: "/El-repo-del-colab/"
+})
